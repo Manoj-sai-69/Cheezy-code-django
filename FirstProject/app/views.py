9f7a938fc1a6dcfdf8604b94d47b90f3af847c61
manoj sai <manojsaichallagulla69@gmail.com>
2026-08-27 22:39:45 +0530

    completely understood the folder structure

--- FirstProject/app/views.py
+++ FirstProject/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def function(request):
+    return HttpResponse("Hey this is manoj here")
+
+def testkwrgs(request,**kwrgs):
+    name=kwrgs.get("name")
+    return HttpResponse(f"<h2>Hey this is to test {name} the kwrgs</h2>")
